88a373d69cc859a9e6ee70af02283923dd2c8e56
Brando Endona <cbrandoendona@gmail.com>
2025-07-14 09:53:12 +0800

    RefreshToken model add  ToBSONMap()

--- internal/auth/model.go
+++ internal/auth/model.go
@@ -1,10 +1,30 @@
 package auth
 
-import "time"
+import (
+	"time"
+
+	"go.mongodb.org/mongo-driver/v2/bson"
+)
 
 type RefreshToken struct {
 	ID        string    `bson:"_id,omitempty"`
 	IsRevoked bool      `bson:"is_revoked"`
 	Token     string    `bson:"token,omitempty"`
 	TTL       time.Time `bson:"ttl,omitempty"`
+}
+
+func (u *RefreshToken) ToBSONMap() (bson.M, error) {
+	var bsonMap bson.M
+
+	filterBye, err := bson.Marshal(u)
+	if err != nil {
+		return nil, err
+	}
+
+	err = bson.Unmarshal(filterBye, &bsonMap)
+	if err != nil {
+		return nil, err
+	}
+
+	return bsonMap, nil
 }
