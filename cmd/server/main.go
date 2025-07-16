12bc4ff94ae4361c9014770663d390b2955363ef
Brando Endona <cbrandoendona@gmail.com>
2025-07-16 15:26:13 +0800

    add ping route

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -86,7 +86,7 @@ func main() {
 	app := fiber.New()
 	app.Use(logger.New(), recover.New())
 
-	app.Get("/kaithheathcheck", func(c *fiber.Ctx) error {
+	app.Get("/ping", func(c *fiber.Ctx) error {
 		return c.SendStatus(200)
 	})
 
