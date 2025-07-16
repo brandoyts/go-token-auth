ce3bc0a1c7f70b9b1a36d48ee18607b829954d5b
Brando Endona <cbrandoendona@gmail.com>
2025-07-16 15:49:02 +0800

    add log message

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -109,5 +109,6 @@ func main() {
 	authRouter.Post("/refresh-token", deps.handler.authHandler.RefreshToken)
 	authRouter.Post("/logout", authChecker(deps.redis, deps.jwtProvider), deps.handler.authHandler.Logout)
 
+	fmt.Println("server is listening on port 8080")
 	app.Listen(":8080")
 }
